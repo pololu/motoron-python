871919fc742261afa609cb45eca57e3c0a8811c7
David (Pololu) <dev-david@pololu.com>
2022-04-28 15:31:24 -0700

    Expanded simple_multi_example.py to support 4 Motorons and added comments.

--- simple_multi_example.py
+++ simple_multi_example.py
@@ -1,38 +1,96 @@
 #!/usr/bin/env python3
 
+# This example shows a simple way to control multiple
+# Motoron Motor Controllers.
+#
+# The motors will stop but automatically recover if:
+# - Motor power (VIN) is interrupted, or
+# - A temporary motor fault occurs, or
+# - A command timeout occurs.
+#
+# This program will terminate if there is an error communicating with any of
+# the Motorons.
+#
+# The motors will stop until you restart this program if any Motoron
+# experiences a reset.
+#
+# If a latched motor fault occurs, the motors experiencing the fault will stop
+# until you power cycle motor power (VIN) or cause the motors to coast.
+
 import time
 import motoron
 
+# Creates an object for each Motoron controller. Each address argument below
+# should be the 7-bit I2C address of the controller.
+#
+# You should use the set_i2c_addresses.py utility to assign a unique address to
+# each Motoron, and then modify the list below to match your setup.
 mc1 = motoron.MotoronI2C(address=17)
 mc2 = motoron.MotoronI2C(address=18)
-
-# TODO: add mc3 and mc4
+mc3 = motoron.MotoronI2C(address=19)
+mc4 = motoron.MotoronI2C(address=20)
 
 def setup_motoron(mc):
   mc.reinitialize()
   mc.disable_crc()
+
+  # Clear the reset flag, which is set after the controller reinitializes
+  # and counts as an error.
   mc.clear_reset_flag()
+
+  # By default, the Motoron is configured to stop the motors if it does not get
+  # a motor control command for 1500 ms.  You can uncomment a line below to
+  # adjust this time or disable the timeout feature.
+  # mc.set_command_timeout_milliseconds(1000)
+  # mc.disable_command_timeout()
 
 setup_motoron(mc1)
 setup_motoron(mc2)
+setup_motoron(mc3)
+setup_motoron(mc4)
 
-mc1.set_max_acceleration(1, 140)
+mc1.set_max_acceleration(1, 80)
 mc1.set_max_deceleration(1, 300)
-mc1.set_max_acceleration(2, 200)
+
+mc1.set_max_acceleration(2, 80)
 mc1.set_max_deceleration(2, 300)
+
 mc1.set_max_acceleration(3, 80)
 mc1.set_max_deceleration(3, 300)
 
-mc2.set_max_acceleration(1, 140)
+mc2.set_max_acceleration(1, 80)
 mc2.set_max_deceleration(1, 300)
-mc2.set_max_acceleration(2, 200)
+
+mc2.set_max_acceleration(2, 80)
 mc2.set_max_deceleration(2, 300)
+
 mc2.set_max_acceleration(3, 80)
 mc2.set_max_deceleration(3, 300)
 
+mc3.set_max_acceleration(1, 80)
+mc3.set_max_deceleration(1, 300)
+
+mc3.set_max_acceleration(2, 80)
+mc3.set_max_deceleration(2, 300)
+
+mc3.set_max_acceleration(3, 80)
+mc3.set_max_deceleration(3, 300)
+
+mc4.set_max_acceleration(1, 80)
+mc4.set_max_deceleration(1, 300)
+
+mc4.set_max_acceleration(2, 80)
+mc4.set_max_deceleration(2, 300)
+
+mc4.set_max_acceleration(3, 80)
+mc4.set_max_deceleration(3, 300)
+
 try:
   while True:
-    changing_speed = 800 if int(time.monotonic() * 1000) & 2048 else -800
+    if int(time.monotonic() * 1000) & 2048:
+      changing_speed = 800
+    else:
+      changing_speed = -800
 
     mc1.set_speed(1, changing_speed)
     mc1.set_speed(2, 100)
@@ -41,6 +99,14 @@ try:
     mc2.set_speed(1, 100)
     mc2.set_speed(2, changing_speed)
     mc2.set_speed(3, -100)
+
+    mc3.set_speed(1, 100)
+    mc3.set_speed(2, -100)
+    mc3.set_speed(3, changing_speed)
+
+    mc4.set_speed(1, changing_speed)
+    mc4.set_speed(2, -changing_speed)
+    mc4.set_speed(3, changing_speed)
 
 except KeyboardInterrupt:
   pass
