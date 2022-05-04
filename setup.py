366dba47602073064db9e0220e0ac6c2d8aecc56
David (Pololu) <dev-david@pololu.com>
2022-05-04 12:59:47 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='motoron_rpi',
+  version='1.0.0',
+  description=(
+    'Library for the Motoron Motor Controllers for Raspberry Pi'
+  ),
+  url='https://github.com/pololu/motoron-rpi',
+  py_modules=['motoron', 'motoron_protocol'],
+  )
+
