6800440c41c072f13002fd033dc8bacb205fa1b5
David (Pololu) <dev-david@pololu.com>
2022-12-16 16:47:58 -0800

    Improve rs485_careful_example.py.

--- rs485_careful_example.py
+++ rs485_careful_example.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python3
 
-# This example shows how to control multiple Motoron Motor Controllers in an
-# RS-485 network while checking for errors.
+# This example shows how to control multiple Motoron Motor Controllers in a
+# half-duplex RS-485 network while checking for errors.
 #
 # This is similar to serial_careful_example.py but it uses the
-# "Multi Device Write" command to efficiently send speeds to multiple Motorons
-# and it uses the "Multi Device Error Check" command to efficiently check for
+# "Multi-device write" command to efficiently send speeds to multiple Motorons
+# and it uses the "Multi-device error check" command to efficiently check for
 # errors on multiple Motorons.
 #
-# You will need to change the following variables to match your configuration:
-# port, starting_device_number, device_count, motors_per_device,
-# communication_options.
+# The error checking code only works if each addressed Motoron can see the
+# repsonses sent by the other Motorons (e.g. they are in a half-duplex RS-485
+# network).
 #
-# Note: If your Motorons have fewer than three motor channels, you should remove
-# the commands that operate on the motors your controller does not have.
-# Otherwise, those commands will cause a protocol error.
+# This code assumes you have configured the Motorons to send 7-bit responses,
+# which is important because it prevents a Motoron from interpreting a response
+# from another Motoron as a command.
+#
+# You will need to change the following settings in this code to match your
+# hardware configuration: port, starting_device_number, device_count,
+# motors_per_device.
 
 import sys
 import time
 import motoron
 import serial
 
-port = serial.Serial("/dev/serial0", 115200, timeout=0.1, write_timeout=0.1)
-
+# Define the range of Motoron device numbers to control.
+# (Note that this code does send some Compact protocol commands which will
+# affect all Motorons regardless of their device number.)
 starting_device_number = 17
 device_count = 3
+
+# Define the number of motor channels per Motoron.
+# It is OK if some of the Motorons in the system have fewer channels than this.
 motors_per_device = 2
 
 communication_options = 1 << motoron.COMMUNICATION_OPTION_7BIT_RESPONSES
 # communication_options |= 1 << motoron.COMMUNICATION_OPTION_14BIT_DEVICE_NUMBER
+
+port = serial.Serial("/dev/serial0", 115200, timeout=0.1, write_timeout=0.1)
 
 mcs = []
 for i in range(device_count):
@@ -75,20 +85,24 @@ speeds = [0] * device_count * motors_per_device
 try:
   while True:
     # Check for errors on all the Motorons.
-    device_error_index = mc_broadcast.multi_device_error_check(
+    error_index = mc_broadcast.multi_device_error_check(
       starting_device_number, device_count)
-    if device_error_index != device_count:
-      mc = mcs[device_error_index]
-      status = mcs[device_error_index].get_status_flags()
-      print("Controller error from device %d: 0x%x" % (mc.device_number, status),
+    if error_index != device_count:
+      mc_broadcast.coast_now()
+      mc = mcs[error_index]
+      status = mcs[error_index].get_status_flags()
+      print("Error from device %d: status 0x%x" % (mc.device_number, status),
         file=sys.stderr)
-      mc_broadcast.reset()
       sys.exit(1)
 
-    # Set the speeds of all the Motorons.
+    # Calculate motor speeds that will drive each motor forward briefly and
+    # repeat every 4 seconds.
     millis = int(time.monotonic() * 1000)
     for i in range(len(speeds)):
-      speeds[i] = 800 if (millis - 512 * i) & 4096 else -800
+      phase = (millis - 512 * i) % 4096
+      speeds[i] = 400 if phase <= 500 else 0
+
+    # Send the motor speeds.
     mc_broadcast.multi_device_write(starting_device_number, device_count,
       motoron.CMD_SET_ALL_SPEEDS, encode_speeds(speeds))
 
